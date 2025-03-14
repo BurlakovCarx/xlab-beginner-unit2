0d5ebda825d29a3917b870562c744072e28fbd23
Ivan Burlakov <i.burlakov@carx-tech.com>
2025-03-14 17:26:56 +0300

    Lesson 2.2

--- xlab-console-app/Program.cs
+++ xlab-console-app/Program.cs
@@ -6,7 +6,37 @@ namespace Xlab
     {
         public static void Main()
         {
-            Console.WriteLine("Hello Aleksey");
+            int a = 1000;
+            for (int i = 1000; i > 0; i -= 7)
+            {
+                Console.WriteLine($"{i}-7 = {i}");
+            }
+
+            Human aleksey = new("Алексей", 27);
+
+            Hello(aleksey);
+        }
+
+        public static void Hello(Human human)
+        {
+            Console.WriteLine($"Привет {human.Name} {2025-human.Age} года рождения");
+        }
+    }
+
+    public class Human
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+
+        public Human(string name, int age)
+        {
+            Name = name;
+
+            if (age <= 0)
+                throw new Exception("Автору 0 лет");
+
+            Age = age;
         }
     }
 }
--- xlab-console-app/obj/Debug/net8.0/xlab-console-app.AssemblyInfo.cs
+++ xlab-console-app/obj/Debug/net8.0/xlab-console-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xlab-console-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5006acbc7f1caad3a020aa2ff09eb7e42405982")]
 [assembly: System.Reflection.AssemblyProductAttribute("xlab-console-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xlab-console-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
